83e210141a7f00ad4b4f5a439e96f319d5fb05fe
GenesisMRL <nunomiglima@gmail.com>
2022-04-05 15:23:29 +0100

    added a couple of tests in rentController

--- src/test/java/com/mindera/school/spaceshiprent/unit/converter/UserConverterTest.java
+++ src/test/java/com/mindera/school/spaceshiprent/unit/converter/UserConverterTest.java
@@ -1,9 +1,9 @@
 package com.mindera.school.spaceshiprent.unit.converter;
 
+import com.mindera.school.spaceshiprent.persistence.entity.SpaceshipEntity;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class UserConverterTest {
-
 }
