1354bcb6c0cdd72a677813082ed2aadbcbe0aad6
jonnym98 <jonasmoutinho5@hotmail.com>
2022-04-12 09:50:01 +0100

    123

--- src/main/java/com/mindera/school/spaceshiprent/converter/UserConverter.java
+++ src/main/java/com/mindera/school/spaceshiprent/converter/UserConverter.java
@@ -3,18 +3,10 @@ package com.mindera.school.spaceshiprent.converter;
 import com.mindera.school.spaceshiprent.dto.user.CreateOrUpdateUserDto;
 import com.mindera.school.spaceshiprent.dto.user.UserDetailsDto;
 import com.mindera.school.spaceshiprent.persistence.entity.UserEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserConverter {
-
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    public UserConverter() {
-        this.bCryptPasswordEncoder = new BCryptPasswordEncoder();
-    }
-
 
     public UserEntity convertToEntity(CreateOrUpdateUserDto dto) {
         return UserEntity.builder()
@@ -24,7 +16,7 @@ public class UserConverter {
                 .licenseNumber(dto.getLicenseNumber())
                 .ssn(dto.getSsn())
                 .planet(dto.getPlanet())
-                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .password(dto.getPassword())
                 .userType(dto.getUserType())
                 .build();
     }
@@ -39,6 +31,7 @@ public class UserConverter {
                 .ssn(entity.getSsn())
                 .planet(entity.getPlanet())
                 .userType(entity.getUserType())
+                .email(entity.getEmail())
                 .build();
     }
 }
