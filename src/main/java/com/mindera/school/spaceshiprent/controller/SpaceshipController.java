536d3d585b7b46c399ef46c0684b832ff1b38426
joao-moutinho <jonasmoutinho5@hotmail.com>
2022-05-10 10:30:34 +0100

    Done Job

--- src/main/java/com/mindera/school/spaceshiprent/controller/SpaceshipController.java
+++ src/main/java/com/mindera/school/spaceshiprent/controller/SpaceshipController.java
@@ -6,8 +6,6 @@ import com.mindera.school.spaceshiprent.service.spaceship.SpaceShipServiceImpl;
 import com.mindera.school.spaceshiprent.util.LoggerMessages;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
